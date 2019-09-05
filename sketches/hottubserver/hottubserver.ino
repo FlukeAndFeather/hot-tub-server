cab9d930c1d37e1b70412d826086c0118ec2f32a
Max Czapanskiy <maxczap@stanford.edu>
2019-09-05 15:18:23 -0700

    Read temperature on command

--- sketches/hottubserver/hottubserver.ino
+++ sketches/hottubserver/hottubserver.ino
@@ -1,22 +1,35 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define INPUT_SZ 10
+
+// DS18B20 data line on pin 4
+#define TS_PIN 4
+OneWire ts_onewire(TS_PIN);
+DallasTemperature ts_dallas(&ts_onewire);
 
 void setup() {
   // Connect to Serial
   Serial.begin(9600);
   while (!Serial);
+
+  // Start Dallas temperature library
+  ts_dallas.begin();
 }
 
 void loop() {
-  char input[INPUT_SZ + 1], comm[INPUT_SZ + 1], arg[INPUT_SZ + 1];
+  char input[INPUT_SZ + 1], command[INPUT_SZ + 1], arg[INPUT_SZ + 1];
   char input_sep[] = ":";
   
   // Check for input
   if (Serial.available() > 0) {
-    readInput(input, comm, arg, input_sep);
-    Serial.println(comm);
-    Serial.println(arg);
+    readInput(input, command, arg, input_sep);
+    if (strcmp(command, "TS") == 0) {
+      float result = readTs();
+      Serial.print("TS:");
+      Serial.println(result);
+    }
   }
 }
 
@@ -32,56 +45,7 @@ void readInput(char* input, char* comm, char* arg, char* sep) {
   strcpy(arg, pch);
 }
 
-  /*
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius;
-
-  // Look for a device
-  if (!ds.search(addr)) {
-    ds.reset_search();
-    delay(250);
-    return;
-  }
-
-  // CRC check to make sure data is valid
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-    Serial.println("CRC is not valid!");
-    return;
-  }
-
-  // Reset the bus, select the device, and start conversion (what's conversion?)
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44);
-  delay(1000);
-
-  // Read scratchpad
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);
-
-  /* 
-   * Why Reset twice? Why do we write 0x44 and 0xBE? Why the delay? 
-   */
-/*
-  // Read 9 bytes
-  for (i = 0; i < 9; i++) {
-    data[i] = ds.read();
-  }
-
-  // Convert data to actual temperature
-  int16_t raw = (data[1] << 8) | data[0];
-  // cfg has something to do with resolution, don't know what it stands for
-  byte cfg = (data[4] & 0x60);
-  if (cfg == 0x00) raw = raw & ~7;
-  else if (cfg == 0x20) raw = raw & ~3;
-  else if (cfg == 0x40) raw = raw & ~1;
-
-  celsius = (float)raw / 16.0;
-  Serial.println(celsius);
+float readTs() {
+  ts_dallas.requestTemperatures();
+  return ts_dallas.getTempCByIndex(0);
 }
-*/
